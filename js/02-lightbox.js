2896469dfaf3c7df825f8facac201f910c84b2ac
YuriyMikh <pamukkale1717@gmail.com>
2023-02-21 19:16:37 +0200

    implemented gallery using SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainerRef.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+function createGalleryMarkup(array) {
+  return array
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+          <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+          </a>
+        </div>`
+    )
+    .join("");
+}
+
+// переопределяем captionDelay и captionsData в момент создания нового экземпляра
+// let gallery = new SimpleLightbox(".gallery a", {
+//   captionDelay: 250,
+//   captionsData: "alt",
+// });
+
+let gallery = new SimpleLightbox(".gallery a")
+// переопределяем captionDelay и captionsData через вызов функции. Это правильно?
+gallery.on("show.simplelightbox", function () {
+  gallery.defaultOptions.captionDelay = 250;
+  gallery.defaultOptions.captionsData = "alt";
+});
